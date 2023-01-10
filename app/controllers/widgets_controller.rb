dddda09dff0bc7f003bccfab45b6232cb471040c
Ahmed Shalaby <kingshalaby@yahoo.com>
2023-01-10 18:57:48 +0200

    add rating partial

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -7,8 +7,21 @@ class WidgetsController < ApplicationController
   ]
   end
   def show
+    manufacturer = OpenStruct.new( 
+      id: rand(100),
+      name: "Sector 7G",
+      address: OpenStruct.new(id: rand(100), country: "UK")
+      )
     @widget = OpenStruct.new(id: params[:id],
-                            manufacturer_id: rand(100),
+                            manufacturer_id: manufacturer.id,
+                            manufacturer: manufacturer,
                             name: "Widget #{params[:id]}")
-    end 
+    def @widget.widget_id
+      if self.id.to_s.length < 3
+        self.id.to_s 
+      else
+        self.id.to_s[0..-3] + "." + self.id.to_s[-2..-1]
+      end 
+    end
+  end 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  
+  def widget_rating_component(widget, suppress_cta: false)
+    render partial: "widgets/rating",
+           locals: { widget: widget, suppress_cta: suppress_cta }
+  end
 end
