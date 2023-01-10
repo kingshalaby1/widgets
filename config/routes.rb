aebb01464c6f3d3b6cea1e1825d5b26b88fc11bb
Ahmed Shalaby <kingshalaby@yahoo.com>
2023-01-10 13:20:21 +0200

    add widget controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  resources :widgets, only: [:show, :index]
+  resources :widget_ratings, only: [ :create ]
+  
+  namespace :customer_service do
+    resources :widgets, only: [ :show, :update, :destroy ]
+    end
+  
+  get "manufacturer/:id", to: "manufacturers#show"
+  
+  
+  ####
+  # Custom routes start here →#
+  # For each new custom route: →#
+   # * Be sure you have the canonical route declared above
+   # * Add the new custom route below the existing ones
+   # * Document why it's needed
+   # * Explain anything else non-standard
+
+   # Used in podcast ads for the 'amazing' campaign
+   get "/amazing", to: redirect("/widgets")
 end
