eaed6103fe35f270b3c42ad7f80cf999ca71f5a0
Ahmed Shalaby <kingshalaby@yahoo.com>
2023-01-10 19:24:33 +0200

    add widget style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
     render partial: "widgets/rating",
            locals: { widget: widget, suppress_cta: suppress_cta }
   end
+  
+  def styled_widget_id(formatted_id) 
+    content_tag(:span,
+    formatted_id,
+    style: "font-family: monospace")
+  end
+
 end
