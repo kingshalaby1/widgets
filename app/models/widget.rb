c5376510392a524be6bc1add8a8f82c5fb08f38f
Ahmed Shalaby <kingshalaby@yahoo.com>
2023-01-27 01:41:40 +0200

    add widget name normalization callback

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -2,4 +2,10 @@ class Widget < ApplicationRecord
   belongs_to :manufacturer
   belongs_to :widget_status
   validates :price_cents, numericality: { less_than_or_equal_to: 10_000_00 }
+  
+  before_validation do
+    if self.name.blank? 
+      self.name = nil
+    end
+  end
 end
